f6fb4987752e8c0a7a3b2cff32aa5c579341d9fd
ismtlee <ismtlee@gmail.com>
2017-09-26 02:02:41 -0400

    .

--- haproxy/docker-entrypoint.sh
+++ haproxy/docker-entrypoint.sh
@@ -4,10 +4,10 @@
 service rsyslog start
 
 # Touch the log file so we can tail on it
-touch /var/log/haproxy.log
+#touch /var/log/haproxy-access.log
 
 # Throw the log to output
-tail -f /var/log/haproxy.log &
+#tail -f /var/log/haproxy-access.log &
 
 # Start haproxy
-exec haproxy -f /usr/local/etc/haproxy/haproxy.cfg
+exec haproxy -d -f /usr/local/etc/haproxy/haproxy.cfg
---
+++ rsyslog/README.md
@@ -0,0 +1,2 @@
+apline rsyslog server
+store and forward use --link mysyslog:graylog and when you start mysyslog use --hostname mysyslog
